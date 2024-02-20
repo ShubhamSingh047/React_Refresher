324748905c379b8491a61fe0bb53b6ea90ea26c4
Shubham <shubham.shubham.singh13@gmail.com>
2024-02-20 18:50:10 +0530

    sorting in js

---
+++ 01-Module Js/sort.js
@@ -0,0 +1,17 @@
+const addDemo=[3,7,10,9,6];
+
+addDemo.sort();
+console.log(addDemo);
+
+const strArr=['a','c','e','d'];
+const sortedStr=strArr.sort();
+
+function Comparable(a,b) {
+    return a-b;
+}
+console.log(sortedStr);
+
+const stringArr=["Banana", "Orange", "Apple", "Mango"];
+stringArr.sort();
+console.log(stringArr);
+
