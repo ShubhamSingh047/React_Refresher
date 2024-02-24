5dcae5d6c006df1691957f6b6089019d33ef2db8
Shubham <shubham.shubham.singh13@gmail.com>
2024-02-24 17:39:21 +0530

    commit done

--- 01-Module Js/Advace_Concept/Async_Await.js
+++ 01-Module Js/Advace_Concept/Async_Await.js
@@ -12,9 +12,42 @@
 
 /* Fetching same using Async/Await */
 
-const apiFun=async()=>{
-    const api=await fetch('https://jsonplaceholder.typicode.com/todos/1');
-    const json=await api.json();
+// const apiFun=async()=>{
+//     const api=await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//     const json=await api.json();
+//     console.log(json);
+// }
+// apiFun();
+
+/* Doing if else using Promises */
+fetch("https://jsonplaceholder.typicode.com/todos/1")
+  .then((response) => response.json())
+  .then((json) => {
+    console.log("json is getting processedd throug promises");
+    if (json.userId == 1) {
+      json.completed == false;
+    } else {
+      json.completed == true;
+    }
     console.log(json);
-}
-apiFun();
+  })
+  .catch((error) => console.log(error));
+
+/* doing if elese in async await */
+const asyFun = async () => {
+  try {
+    const apiCall = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const json = await apiCall.json();
+    console.log("test is getting processed via async call");
+    if (json.userId == 1) {
+      json.completed == true;
+    } else {
+      json.completed == false;
+    }
+    console.log(json);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+asyFun();
