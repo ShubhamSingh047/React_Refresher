caf9151701c690c540ee2685ed176bfadc85049f
Shubham <shubham.shubham.singh13@gmail.com>
2024-02-28 23:52:59 +0530

    prototype

--- 01-Module Js/Call_Apply_Bind/object.js
+++ 01-Module Js/Call_Apply_Bind/object.js
@@ -1,3 +1,22 @@
-const test = {
-  test: "123",
+//lets expolore functionality of prototype
+
+function createUser(username, score) {
+  this.username = username;
+  this.score = score;
+}
+
+createUser.prototype.increament = function () {
+  score++;
+  console.log(`score is ${this.score}`);
 };
+
+createUser.prototype.showUser = function () {
+  console.log(`Hello ${this.username}`);
+};
+
+//we are using new keyword to create instance of test & finalTest;
+const test = new createUser("shubham", 80);
+const finalTest = new createUser("Shubham", 85);
+
+test.increament();
+test.showUser();
