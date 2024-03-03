12e95622de0ee6165d0c75618d02d19da01260b7
Shubham <shubham.shubham.singh13@gmail.com>
2024-03-04 01:23:25 +0530

    object properties

--- 01-Module Js/Call_Apply_Bind/object.js
+++ 01-Module Js/Call_Apply_Bind/object.js
@@ -31,18 +31,47 @@ test.increament();
 test.showUser();
 
 /* >>>>>>>>>------------------Object Methods----------------<<<<<<<<<<<<< */
-let obj={
-    name:"shubham",
-    class:"graduated",
-}
+let obj = {
+  name: "shubham",
+  class: "graduated",
+};
 
 console.log(`getting only keys of an Object`);
 
-const keys=Object.keys(obj);
+const keys = Object.keys(obj);
 console.log(keys);
 
-const values=Object.values(obj);
+const values = Object.values(obj);
 console.log(values);
 
-const array_of_both=Object.entries(obj);
+const array_of_both = Object.entries(obj);
 console.log(array_of_both);
+
+// Oops and new key word
+
+const user = {
+  userName: "shubham",
+  loginCount: 9,
+  signIn: true,
+};
+
+/*
+
+## Function Invocation: When a function is called as a standalone function (not as a method of an object),
+         this typically refers to the global object in non-strict mode and undefined in strict mode.
+
+## Method Invocation: When a function is called as a method of an object, this refers to the object that owns the method.
+
+## Constructor Invocation: When a function is called with the new keyword, this refers to a newly created object.
+
+*/
+
+function User(userName) {
+  this.userName = userName; //here due to this in GEC of function scope a property name userName is created !
+  console.log(`Hi ${userName} !`);
+}
+
+const objUser = new User("Shivam");
+const objUser2 = new User("Shubham");
+
+console.log(objUser);
