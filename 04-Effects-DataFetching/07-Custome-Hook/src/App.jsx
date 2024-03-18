030151c86ac26d543437bfa7da79458664b00c77
Shubham <shubham.shubham.singh13@gmail.com>
2024-03-19 04:24:59 +0530

    useReduce hook

--- 04-Effects-DataFetching/07-Custome-Hook/src/App.jsx
+++ 04-Effects-DataFetching/07-Custome-Hook/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 // import DateCounter from "./starter/DateCounter";
-import DateCounter from "./DateCounter";
+import DateCounterRed from "./DateCounter";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      {/* <DateCounter /> */}
-      <DateCounter />
+      <DateCounterRed />
     </>
   );
 }
--- 04-Effects-DataFetching/07-Custome-Hook/src/DateCounter.jsx
+++ 04-Effects-DataFetching/07-Custome-Hook/src/DateCounter.jsx
@@ -1,25 +1,71 @@
 import React, { useState } from "react";
+import { useReducer } from "react";
 
-const DateCounter = () => {
+const reducer = (state, action) => {
+  console.log(state, action);
+  switch (action.type) {
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "setCount":
+      return { ...state, count: action.payload };
+    case "setStep":
+      return { ...state, step: action.payload };
+    case "reset":
+      return action.payload;
+    default:
+      throw new Error("Unknown action !");
+  }
+};
+
+const DateCounterRed = () => {
+  const intialState = { count: 0, step: 1 };
+
+  const [state, dispatch] = useReducer(reducer, intialState);
+  const { count, step } = state;
   const date = new Date();
-  console.log(date, "date");
-  const [count, setCount] = useState(1);
-  //   const [date, setDate] = useState(0);
+  date.setDate(date.getDate() + count);
+
+  const handleCountChange = (e) => {
+    dispatch({ type: "setCount", payload: +e.target.value });
+    // setDate(+e.target.value);
+  };
+
+  const handleStepChange = (e) =>
+    dispatch({
+      type: "setStep",
+      payload: Number(e.target.value),
+    });
+
+  const inc = () => {
+    dispatch({ type: "inc", payload: 1 });
+  };
+
+  const dec = () => {
+    dispatch({ type: "dec", payload: -1 });
+  };
+
+  const handleReset = () => {
+    dispatch({ type: "reset", payload: intialState });
+  };
+
   return (
-    <>
+    <div>
       <div>
-        <input
-          type="range"
-          value={count}
-          onChange={(e) => setCount(Number(e.target.value))}
-          max={10}
-        />
-        <p>{count}</p>
-        {/* <input /> */}
+        <input type="range" value={step} onChange={handleStepChange} max={10} />
+        <p>{step}</p>
+        <button onClick={dec}>dec -</button>
+        <input value={count} onChange={handleCountChange} />
+        <button onClick={inc}>inc +</button>
       </div>
-      <div>DateCounter</div>
-    </>
+
+      <p>{date.toDateString()}</p>
+      <div>
+        <button onClick={handleReset}>reset</button>
+      </div>
+    </div>
   );
 };
 
-export default DateCounter;
+export default DateCounterRed;
